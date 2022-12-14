0151411d12a2939fa18b8e2d3a555773259e4138
Vethariel <dagraciap@unal.edu.co>
2022-12-13 23:49:34 -0500

    Basic square movement

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import pygame, sys
+import pygame, sys, os
 
 class Game():
     
     def __init__(self) -> None:
-         pass
+         self.x = 100
+         self.y = 100
     
     def init(self):
+        
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+        
         pygame.init()
         self.screen = pygame.display.set_mode((500,500))
 
@@ -13,12 +17,23 @@ class Game():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.y -= 10
+            elif event.key == pygame.K_DOWN:
+                self.y += 10
+            elif event.key == pygame.K_RIGHT:
+                self.x += 10
+            elif event.key == pygame.K_LEFT:
+                self.x -= 10
     
-    def update():
+    def update(self):
         pass
     
-    def render():
-        pass
+    def render(self):
+        self.screen.fill((0,0,0))
+        pygame.draw.rect(self.screen,(0,255,0),(self.x,self.y,100,100))
+        pygame.display.update()
 
     def main(self):
         self.init()
