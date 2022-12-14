387f3deca0538771dad4a9debb8f047830e1690e
Vethariel <dagraciap@unal.edu.co>
2022-12-13 23:23:24 -0500

    Class and game loop pattern

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-import pygame
+import pygame, sys
 
-pygame.init()
+class Game():
+    
+    def __init__(self) -> None:
+         pass
+    
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((500,500))
 
-screen = pygame.display.set_mode((500,500))
-
-while True:
-    for event in pygame.event.get():
+    def input(self, event):
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
+    
+    def update():
+        pass
+    
+    def render():
+        pass
+
+    def main(self):
+        self.init()
+        while True:
+            for event in pygame.event.get():
+                self.input(event)
+            self.update()
+            self.render()
+
+if __name__=="__main__":
+    game = Game()
+    game.main()
